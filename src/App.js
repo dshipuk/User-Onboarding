aec49905a4e7805f31f71b399538ac68a782149b
unknown <dshipuk@yahoo.com>
2022-03-30 13:58:44 -0700

    added change handler submit handler and tested if its working seems to be

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Form from "./Components/Form";
 
-
+const initialFormVals = {
+  name: "",
+  email: "",
+  password: "",
+  tos: false,
+}
 
 
 function App() {
+  const [values, setValues] = useState(initialFormVals)
+
+  const onChange = (name, value) => {
+    setValues({ ...values, [name]: value })
+  }
+
+  const onSub = () => {
+    setValues(initialFormVals)
+  }
+
   return (
     <div className="App">
       <h1>Form</h1>
       <Form 
-        
+        values={values}
+        change={onChange}
+        submit={onSub}
       />
     </div>
   );
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,12 +1,56 @@
 import React from "react";
 
 
-const Form = () => {
+const Form = (props) => {
 
 
+    const handleChange = evt => {
+        const { name, value } = evt.target;
+
+        props.change(name, value)
+    }
+
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        props.submit()
+    }
 
     return(
-        <div>Form Return</div>
+        <form onSubmit={handleSubmit}>
+            <label>Name:
+                <input 
+                    type="text"
+                    value={props.values.name}
+                    name="name"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Email:
+                <input 
+                    type="email"
+                    value={props.values.email}
+                    name="email"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Password:
+                <input 
+                    type="password"
+                    values={props.values.password}
+                    name="password"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>TOS
+                <input 
+                    type="checkbox"
+                    value={props.values.tos}
+                    name="tos"
+                    onChange={handleChange}
+                />
+            </label>
+            <input type="submit" value="create team"/>
+        </form>
     )
 }
 
